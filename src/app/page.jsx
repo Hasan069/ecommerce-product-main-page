e3c944ebe12c263e7c15b5f8a7343377602e1b22
Cyanider <amihasan420@gmail.com>
2024-09-15 14:02:51 +0600

    day 2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,26 @@
+import React from "react";
 import Navbar from "./components/Navbar";
-import logo from "../../public/logo.svg";
-import {cart} from "../../public/icon-cart.svg";
+import Image from "next/image";
+import Logo from "../../public/logo.svg";
+import cart from "../../public/icon-cart.svg";
 import avatar from "../../public/image-avatar.png";
 
 export default function Home() {
   return (
-    <div className="flex">
-      <logo/>
-      <Navbar />
-      <cart />
-      <avatar />
-    </div>
+    <>
+      <div>
+        <div className="flex justify-around m-6  relative">
+          <Image src={Logo} alt="Logo" className="mt-4 h-6" />
+          <Navbar />
+          <div className="gap-12 flex">
+            <Image src={cart} alt="Cart Icon" className="h-5 left-12 mt-6" />
+            <Image src={avatar} alt="User Avatar" className="h-12 w-12" />
+          </div>
+        </div>
+
+
+        
+      </div>
+    </>
   );
 }
